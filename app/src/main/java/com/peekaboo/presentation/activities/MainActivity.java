8a428f3f4d2642376cd719ab265d7c18e4b7b06c
Nikita Romanenko <nikita241296@gmail.com>
2016-07-12 00:39:07 +0300

    bt callback

--- app/src/main/java/com/peekaboo/presentation/activities/MainActivity.java
+++ app/src/main/java/com/peekaboo/presentation/activities/MainActivity.java
@@ -5,8 +5,12 @@ import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.widget.Toast;
 
 import com.peekaboo.R;
+
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -14,7 +18,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        ButterKnife.bind(this);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
@@ -24,5 +28,10 @@ public class MainActivity extends AppCompatActivity {
                 this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
         drawer.setDrawerListener(toggle);
         toggle.syncState();
+    }
+
+    @OnClick(R.id.bt)
+    public void btClicked(){
+        Toast.makeText(getApplicationContext(), "Button", Toast.LENGTH_LONG).show();
     }
 }
