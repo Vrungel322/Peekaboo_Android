06e641d2e0f5f67221cfc2e54b085553558aac03
Nikita Romanenko <nikita241296@gmail.com>
2016-07-12 22:43:27 +0300

    transpearancy and render script add

--- app/src/main/java/com/peekaboo/presentation/activities/MainActivity.java
+++ app/src/main/java/com/peekaboo/presentation/activities/MainActivity.java
@@ -1,5 +1,7 @@
 package com.peekaboo.presentation.activities;
 
+import android.graphics.Color;
+import android.os.Build;
 import android.os.Bundle;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
@@ -27,6 +29,8 @@ public class MainActivity extends AppCompatActivity {
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
                 this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
         drawer.setDrawerListener(toggle);
+        drawer.setStatusBarBackgroundColor(
+                getResources().getColor(R.color.colorAccent));
         toggle.syncState();
     }
 
