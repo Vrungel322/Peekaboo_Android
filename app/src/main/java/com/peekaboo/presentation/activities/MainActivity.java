141bfd2804ec704a8e1d211e206e0ae34cf66bd3
Arkadii <arkadii.humennyi@buki.com.ua>
2016-08-11 10:15:02 +0300

    refactor drawer item selection

--- app/src/main/java/com/peekaboo/presentation/activities/MainActivity.java
+++ app/src/main/java/com/peekaboo/presentation/activities/MainActivity.java
@@ -15,13 +15,10 @@ import android.widget.Toast;
 
 import com.peekaboo.R;
 import com.peekaboo.presentation.PeekabooApplication;
-import com.peekaboo.presentation.di.ApplicationComponent;
 import com.peekaboo.presentation.fragments.CallsFragment;
 import com.peekaboo.presentation.fragments.ContactsFragment;
 import com.peekaboo.presentation.fragments.DialogsFragment;
 import com.peekaboo.presentation.fragments.ProfileFragment;
-import com.peekaboo.presentation.fragments.RecordTestFragment;
-import com.peekaboo.presentation.fragments.ServiceTestFragment;
 import com.peekaboo.presentation.fragments.SettingsFragment;
 import com.peekaboo.presentation.utils.ResourcesUtils;
 
@@ -43,30 +40,6 @@ public class MainActivity extends AppCompatActivity {
     LinearLayout llProfile;
     @BindView(R.id.llSettings)
     LinearLayout llSettings;
-    @BindView(R.id.ivIconChat)
-    ImageView ivIconChat;
-    @BindView(R.id.tvTextChat)
-    TextView tvTextChat;
-    @BindView(R.id.ivIconCalls)
-    ImageView ivIconCalls;
-    @BindView(R.id.tvTextCalls)
-    TextView tvTextCalls;
-    @BindView(R.id.ivIconContacts)
-    ImageView ivIconContacts;
-    @BindView(R.id.tvTextContacts)
-    TextView tvTextContacts;
-    @BindView(R.id.ivIconProfile)
-    ImageView ivIconProfile;
-    @BindView(R.id.tvTextProfile)
-    TextView tvTextProfile;
-    @BindView(R.id.ivIconSettings)
-    ImageView ivIconSettings;
-    @BindView(R.id.tvTextSettings)
-    TextView tvTextSettings;
-    @BindView(R.id.ivIconExit)
-    ImageView ivIconExit;
-    @BindView(R.id.tvTextExit)
-    TextView tvTextExit;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -89,125 +62,56 @@ public class MainActivity extends AppCompatActivity {
     }
 
     @OnClick({R.id.llDialogs, R.id.llCalls, R.id.llContacts, R.id.llProfile, R.id.llSettings})
-    public void onDraverItemClick(View v){
+    public void onDraverItemClick(View v) {
         selectionMode(v.getId());
-        switch (v.getId()){
-            case R.id.llDialogs:{
+        switch (v.getId()) {
+            case R.id.llDialogs:
                 changeFragment(new DialogsFragment(), "dialogsFragment");
                 break;
-            }
-            case R.id.llCalls:{
+            case R.id.llCalls:
                 changeFragment(new CallsFragment(), "callsFragment");
                 break;
-            }
-            case R.id.llContacts:{
+            case R.id.llContacts:
                 changeFragment(new ContactsFragment(), "contactsFragment");
                 break;
-            }
-            case R.id.llProfile:{
+            case R.id.llProfile:
                 changeFragment(new ProfileFragment(), "profileFragment");
                 break;
-            }
-            case R.id.llSettings:{
+            case R.id.llSettings:
                 changeFragment(new SettingsFragment(), "settingsFragment");
                 break;
-            }
         }
 
     }
 
     @OnClick(R.id.llExit)
-    public void lvExitClick(){
+    public void lvExitClick() {
         Toast.makeText(getApplicationContext(), "Log out", Toast.LENGTH_LONG).show();
     }
 
-    private void selectionMode(int id){
-        if(id == R.id.llDialogs){
-            llDialogs.setSelected(true);
-            llCalls.setSelected(false);
-            llContacts.setSelected(false);
-            llProfile.setSelected(false);
-            llSettings.setSelected(false);
-            tvTextChat.setTextColor(ResourcesUtils.getColor(this, R.color.colorAccent));
-            tvTextCalls.setTextColor(ResourcesUtils.getColor(this, R.color.textColorHint));
-            tvTextContacts.setTextColor(ResourcesUtils.getColor(this, R.color.textColorHint));
-            tvTextProfile.setTextColor(ResourcesUtils.getColor(this, R.color.textColorHint));
-            tvTextSettings.setTextColor(ResourcesUtils.getColor(this, R.color.textColorHint));
-            ivIconChat.setImageResource(R.drawable.chats_green);
-            ivIconCalls.setImageResource(R.drawable.calls_grey);
-            ivIconContacts.setImageResource(R.drawable.contacts_grey);
-            ivIconProfile.setImageResource(R.drawable.profile_grey);
-            ivIconSettings.setImageResource(R.drawable.settings_grey);
+    private void selectionMode(int id) {
+        llDialogs.setSelected(false);
+        llCalls.setSelected(false);
+        llContacts.setSelected(false);
+        llProfile.setSelected(false);
+        llSettings.setSelected(false);
+        switch (id) {
+            case R.id.llDialogs:
+                llDialogs.setSelected(true);
+                break;
+            case R.id.llCalls:
+                llCalls.setSelected(true);
+                break;
+            case R.id.llContacts:
+                llContacts.setSelected(true);
+                break;
+            case R.id.llProfile:
+                llProfile.setSelected(true);
+                break;
+            case R.id.llSettings:
+                llSettings.setSelected(true);
+                break;
         }
-        if(id == R.id.llCalls){
-            llDialogs.setSelected(false);
-            llCalls.setSelected(true);
-            llContacts.setSelected(false);
-            llProfile.setSelected(false);
-            llSettings.setSelected(false);
-            tvTextChat.setTextColor(ResourcesUtils.getColor(this, R.color.textColorHint));
-            tvTextCalls.setTextColor(ResourcesUtils.getColor(this, R.color.colorAccent));
-            tvTextContacts.setTextColor(ResourcesUtils.getColor(this, R.color.textColorHint));
-            tvTextProfile.setTextColor(ResourcesUtils.getColor(this, R.color.textColorHint));
-            tvTextSettings.setTextColor(ResourcesUtils.getColor(this, R.color.textColorHint));
-            ivIconChat.setImageResource(R.drawable.chats_grey);
-            ivIconCalls.setImageResource(R.drawable.calls_green);
-            ivIconContacts.setImageResource(R.drawable.contacts_grey);
-            ivIconProfile.setImageResource(R.drawable.profile_grey);
-            ivIconSettings.setImageResource(R.drawable.settings_grey);
-        }
-        if(id == R.id.llContacts){
-            llDialogs.setSelected(false);
-            llCalls.setSelected(false);
-            llContacts.setSelected(true);
-            llProfile.setSelected(false);
-            llSettings.setSelected(false);
-            tvTextChat.setTextColor(ResourcesUtils.getColor(this, R.color.textColorHint));
-            tvTextCalls.setTextColor(ResourcesUtils.getColor(this, R.color.textColorHint));
-            tvTextContacts.setTextColor(ResourcesUtils.getColor(this, R.color.colorAccent));
-            tvTextProfile.setTextColor(ResourcesUtils.getColor(this, R.color.textColorHint));
-            tvTextSettings.setTextColor(ResourcesUtils.getColor(this, R.color.textColorHint));
-            ivIconChat.setImageResource(R.drawable.chats_grey);
-            ivIconCalls.setImageResource(R.drawable.calls_grey);
-            ivIconContacts.setImageResource(R.drawable.contacts_green);
-            ivIconProfile.setImageResource(R.drawable.profile_grey);
-            ivIconSettings.setImageResource(R.drawable.settings_grey);
-        }
-        if(id == R.id.llProfile){
-            llDialogs.setSelected(false);
-            llCalls.setSelected(false);
-            llContacts.setSelected(false);
-            llProfile.setSelected(true);
-            llSettings.setSelected(false);
-            tvTextChat.setTextColor(ResourcesUtils.getColor(this, R.color.textColorHint));
-            tvTextCalls.setTextColor(ResourcesUtils.getColor(this, R.color.textColorHint));
-            tvTextContacts.setTextColor(ResourcesUtils.getColor(this, R.color.textColorHint));
-            tvTextProfile.setTextColor(ResourcesUtils.getColor(this, R.color.colorAccent));
-            tvTextSettings.setTextColor(ResourcesUtils.getColor(this, R.color.textColorHint));
-            ivIconChat.setImageResource(R.drawable.chats_grey);
-            ivIconCalls.setImageResource(R.drawable.calls_grey);
-            ivIconContacts.setImageResource(R.drawable.contacts_grey);
-            ivIconProfile.setImageResource(R.drawable.profile_green);
-            ivIconSettings.setImageResource(R.drawable.settings_grey);
-        }
-        if(id == R.id.llSettings){
-            llDialogs.setSelected(false);
-            llCalls.setSelected(false);
-            llContacts.setSelected(false);
-            llProfile.setSelected(false);
-            llSettings.setSelected(true);
-            tvTextChat.setTextColor(ResourcesUtils.getColor(this, R.color.textColorHint));
-            tvTextCalls.setTextColor(ResourcesUtils.getColor(this, R.color.textColorHint));
-            tvTextContacts.setTextColor(ResourcesUtils.getColor(this, R.color.textColorHint));
-            tvTextProfile.setTextColor(ResourcesUtils.getColor(this, R.color.textColorHint));
-            tvTextSettings.setTextColor(ResourcesUtils.getColor(this, R.color.colorAccent));
-            ivIconChat.setImageResource(R.drawable.chats_grey);
-            ivIconCalls.setImageResource(R.drawable.calls_grey);
-            ivIconContacts.setImageResource(R.drawable.contacts_grey);
-            ivIconProfile.setImageResource(R.drawable.profile_grey);
-            ivIconSettings.setImageResource(R.drawable.settings_green);
-        }
-
     }
 
     private void changeFragment(Fragment fragment, String tag) {
