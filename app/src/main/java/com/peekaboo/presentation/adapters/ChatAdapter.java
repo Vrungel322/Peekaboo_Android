0b74103287131675151ea3f49eeafb5f9365ddfb
Nataliia Hrechushkina <allana437@gail.com>
2016-09-02 18:10:09 +0200

    refactor (reverted from commit 06e2137b29a7869207d1e3f1beb22b7d64e5046a)

--- app/src/main/java/com/peekaboo/presentation/adapters/ChatAdapter.java
+++ app/src/main/java/com/peekaboo/presentation/adapters/ChatAdapter.java
@@ -243,9 +243,9 @@ public class ChatAdapter extends RecyclerView.Adapter<ChatAdapter.ViewHolder>
 
     public void swithPlayButtonImage(RecyclerView.ViewHolder holder, boolean toPlay){
         if(toPlay){
-            ((ViewHolderAudio) holder).ibPlayRecord.setBackgroundResource(R.drawable.play_green);
+            ((ViewHolderAudio) holder).ibPlayRecord.setBackgroundResource(R.drawable.play_blue);
         } else {
-            ((ViewHolderAudio) holder).ibPlayRecord.setBackgroundResource(R.drawable.pause_green);
+            ((ViewHolderAudio) holder).ibPlayRecord.setBackgroundResource(R.drawable.pause_blue);
         }
     }
 
