f447cb76adddbf1a3ed4ee263f13b8fd2f4bd90b
st1ch <a.e.getman@gmail.com>
2016-09-12 16:56:43 +0300

    deleted old classes; added listener to recorder;

--- app/src/main/java/com/peekaboo/domain/AudioRecorder.java
+++ app/src/main/java/com/peekaboo/domain/AudioRecorder.java
@@ -6,32 +6,50 @@ import rx.schedulers.Schedulers;
 /**
  * Created by st1ch on 28.07.2016.
  */
-public class AudioRecorder implements IAudioRecorder {
+public class AudioRecorder {
 
     private Record record;
+    private OnStartRecordingListener onStartRecordingListener;
+    private OnStopRecordingListener onStopRecordingListener;
 
     public AudioRecorder(Record record) {
         this.record = record;
     }
 
-    @Override
     public Observable<Record> startRecording() {
+        onStartRecordingListener.onStartRecording();
         return Observable.just(record)
                 .subscribeOn(Schedulers.computation())
                 .map(record -> {
-            record.startRecording();
-            return record;
-        });
+                    record.startRecording();
+                    return record;
+                });
     }
 
-    @Override
     public Observable<Record> stopRecording() {
         return Observable.just(record)
                 .subscribeOn(Schedulers.computation())
                 .map(record -> {
+                    onStopRecordingListener.onStopRecording();
                     record.stopRecording();
                     return record;
                 });
+    }
+
+    public void setOnStartRecordingListener(OnStartRecordingListener listener){
+        onStartRecordingListener = listener;
+    }
+
+    public void setOnStopRecordingListener(OnStopRecordingListener listener){
+        onStopRecordingListener = listener;
+    }
+
+    public interface OnStartRecordingListener {
+        void onStartRecording();
+    }
+
+    public interface OnStopRecordingListener {
+        void onStopRecording();
     }
 
 }
