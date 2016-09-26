a21ca76c708e5d0332ee1213fb05cb660f1d2ad9
patrikstar <patrikstarman@gmail.com>
2016-09-26 16:09:53 +0300

    temp commit

--- app/src/main/java/com/peekaboo/presentation/adapters/DialogsLargeAdapter.java
+++ app/src/main/java/com/peekaboo/presentation/adapters/DialogsLargeAdapter.java
@@ -4,6 +4,8 @@ import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageButton;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.peekaboo.R;
@@ -11,7 +13,7 @@ import com.peekaboo.R;
 import java.util.ArrayList;
 import java.util.List;
 
-public final class DialogsLargeAdapter extends RecyclerView.Adapter<DialogsLargeAdapter.ViewHolder> {
+public final class DialogsLargeAdapter extends RecyclerView.Adapter<DialogsLargeAdapter.ViewHolder>  {
     private final List<String> items;
 
     public DialogsLargeAdapter(ArrayList<String> item) {
@@ -31,6 +33,7 @@ public final class DialogsLargeAdapter extends RecyclerView.Adapter<DialogsLarge
     public void onBindViewHolder(ViewHolder holder, int position) {
         String text = items.get(position);
         holder.setText(text);
+
     }
 
 
@@ -40,15 +43,25 @@ public final class DialogsLargeAdapter extends RecyclerView.Adapter<DialogsLarge
     }
 
     public static final class ViewHolder extends RecyclerView.ViewHolder {
-        private final TextView textView;
+        private  TextView dialogname, dialogprew, num_mes, date;
+        private ImageView trash, star, sound, contact, status ;
 
         private ViewHolder(View itemView) {
             super(itemView);
-            this.textView = (TextView) itemView.findViewById(R.id.tvDialog_name);
+            dialogname = (TextView) itemView.findViewById(R.id.tvDialog_name);
+            dialogprew = (TextView) itemView.findViewById(R.id.tvDialog_preview);
+            trash = (ImageView) itemView.findViewById(R.id.iv_swipetrash);
+            star = (ImageView) itemView.findViewById(R.id.iv_swipestar);
+            sound = (ImageView) itemView.findViewById(R.id.iv_swipenosound);
+            contact = (ImageView) itemView.findViewById(R.id.ivDialog_contact);
+            num_mes = (TextView) itemView.findViewById(R.id.tv_num_mes);
+            status = (ImageView) itemView.findViewById(R.id.ivDialog_status);
+            date = (TextView) itemView.findViewById(R.id.tvDialog_time_date);
+
         }
 
         public void setText(CharSequence text) {
-            textView.setText(text);
+            dialogname.setText(text);
         }
     }
 }
