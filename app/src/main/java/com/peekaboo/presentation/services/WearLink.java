6748fc6052c0afac12e1b5536cea680249adf493
Maxim Kizub <mkizub@gmail.com>
2016-10-09 00:26:06 +0300

    Changes to chat requests

--- app/src/main/java/com/peekaboo/presentation/services/WearLink.java
+++ app/src/main/java/com/peekaboo/presentation/services/WearLink.java
@@ -7,20 +7,25 @@ import android.os.DeadObjectException;
 import android.os.Handler;
 import android.os.IBinder;
 import android.os.Looper;
+import android.os.Message;
 import android.os.RemoteException;
 import android.util.Log;
 
+import com.peekaboo.data.repositories.database.contacts.Contact;
 import com.peekaboo.data.repositories.database.messages.PMessage;
+import com.peekaboo.data.repositories.database.messages.PMessageAbs;
 import com.peekaboo.domain.AccountUser;
 import com.peekaboo.domain.Record;
+import com.peekaboo.domain.SessionRepository;
 import com.peekaboo.presentation.PeekabooApplication;
-import com.peekaboo.utils.Constants;
 
+import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.io.File;
+import java.io.FileInputStream;
 import java.io.FileOutputStream;
-import java.io.IOException;
 import java.util.List;
 import java.util.Map;
 import java.util.concurrent.CopyOnWriteArrayList;
@@ -39,46 +44,170 @@ public class WearLink extends Service implements IMessenger.MessengerListener
     IMessenger notifier;
     @Inject
     AccountUser accountUser;
+    @Inject
+    SessionRepository sessionRepository;
 
     private final List<ChatRequestImpl> chatRequests = new CopyOnWriteArrayList<ChatRequestImpl>();
 
-    private Handler handler = new Handler(Looper.getMainLooper());
+    private static final int MSG_POST_CHAT_MESSAGE = 1;
+
+    private Handler handler = new Handler(Looper.getMainLooper()) {
+
+        @Override
+        public void handleMessage(Message msg) {
+            switch (msg.what) {
+            case MSG_POST_CHAT_MESSAGE:
+                notifier.sendMessage((PMessage) msg.obj);
+                return;
+            }
+        }
+    };
+
+    private static final String predefinedContacts =
+            "[" +
+            "   {" +
+            "      \"id\":\"386\"," +
+            "      \"name\":\"pomaranch\"" +
+            "   }," +
+            "   {" +
+            "      \"id\":\"480\"," +
+            "      \"name\":\"Nikitos\"" +
+            "   }," +
+            "   {" +
+            "      \"id\":\"537\"," +
+            "      \"name\":\"ac2epsilon\"" +
+            "   }," +
+            "   {" +
+            "      \"id\":\"448\"," +
+            "      \"name\":\"ikasyk\"" +
+            "   }," +
+            "   {" +
+            "      \"id\":\"476\"," +
+            "      \"name\":\"Arkadius\"" +
+            "   }," +
+            "   {" +
+            "      \"id\":\"578\"," +
+            "      \"name\":\"patrikstarman\"" +
+            "   }," +
+            "   {" +
+            "      \"id\":\"121\"," +
+            "      \"name\":\"AidenEagleton\"" +
+            "   }," +
+            "   {" +
+            "      \"id\":\"202\"," +
+            "      \"name\":\"JohnEagleton\"" +
+            "   }," +
+            "   {" +
+            "      \"id\":\"629\"," +
+            "      \"name\":\"userone\"" +
+            "   }," +
+            "   {" +
+            "      \"id\":\"138\"," +
+            "      \"name\":\"Vrungel\"" +
+            "   }," +
+            "   {" +
+            "      \"id\":\"140\"," +
+            "      \"name\":\"Oleksii\"" +
+            "   }," +
+            "   {" +
+            "      \"id\":\"160\"," +
+            "      \"name\":\"Vozhak\"" +
+            "   }," +
+            "   {" +
+            "      \"id\":\"166\"," +
+            "      \"name\":\"st1ch\"" +
+            "   }" +
+            "]";
 
     class ChatRequestImpl extends ChatRequest.Stub {
         ChatListener listener;
 
         @Override
-        public String post(String action, Map params, byte[] data) throws RemoteException {
-            if (params == null)
-                return null;
-            if (!"22050".equals(params.get("rate")))
-                return null;
-            if (!"PCM16".equals(params.get("fmt")))
-                return null;
-            Record record = new Record(accountUser.getId());
-            long totalAudioLen = data.length;
-            long totalDataLen = totalAudioLen + 36;
-            long longSampleRate = 22050;
-            int channels = 1;
-            long byteRate = 16 * longSampleRate * channels / 8;
-            try {
-                FileOutputStream out = new FileOutputStream(record.getFilename());
-                record.WriteWaveFileHeader(out, totalAudioLen, totalDataLen, longSampleRate, 1, byteRate);
-                out.write(data);
-                out.close();
-            } catch (IOException e) {
-                Log.e(TAG, "Error saving wav file", e);
-            }
-            final PMessage message = new PMessage(true, PMessage.PMESSAGE_MEDIA_TYPE.AUDIO_MESSAGE,
-                    record.getFilename(), System.currentTimeMillis(),
-                    PMessage.PMESSAGE_STATUS.STATUS_SENT,
-                    accountUser.getId(), accountUser.getId());
-            handler.post(new Runnable() {
-                @Override
-                public void run() {
-                    notifier.sendMessage(message);
+        public String post(String action, Map params, String data) throws RemoteException {
+            if ("attach".equals(action)) {
+                try {
+                    JSONObject json = new JSONObject();
+                    json.put("id", accountUser.getId());
+                    json.put("name", accountUser.getUsername());
+                    json.put("contacts", new JSONArray(predefinedContacts));
+//                    final JSONArray jarr = new JSONArray();
+//                    sessionRepository.getAllSavedContacts().forEach(contacts -> {
+//                        for (Contact c : contacts) {
+//                            try {
+//                                JSONObject jc = new JSONObject();
+//                                jc.put("id", c.contactId());
+//                                jc.put("name", c.contactNickname());
+//                                jarr.put(jc);
+//                            } catch (JSONException e) {}
+//                        }
+//                        Log.e(TAG, "contacts: " + jarr);
+//                    });
+                    return json.toString();
+                } catch (Exception e) {
+                    Log.e(TAG, "Error saving wav file", e);
                 }
-            });
+                return null;
+            }
+            if ("post".equals(action)) {
+                try {
+                    PMessage message = null;
+                    if (params.containsKey("audio")) {
+                        int rate = Integer.parseInt((String) params.get("rate"));
+                        if (rate < 8000 || rate > 44100) {
+                            Log.e(TAG, "Expecting sampkle rate between 8000 and 44100, got " + rate);
+                            return null;
+                        }
+                        if (!"PCM16".equals(params.get("fmt"))) {
+                            Log.e(TAG, "Expecting PCM16 fmt, got " + params.get("fmt"));
+                            return null;
+                        }
+                        JSONObject json = new JSONObject(data);
+                        long timestamp = json.optLong("timestamp", System.currentTimeMillis());
+                        String receiverId = json.optString("receiver", accountUser.getId());
+                        String senderId = json.optString("sender", accountUser.getId());
+                        File file = new File((String) params.get("audio"));
+                        if (!file.exists() || !file.canRead() || !file.isFile() || file.length() == 0) {
+                            Log.e(TAG, "File not exists or not readable: " + file);
+                            return null;
+                        }
+                        Record record = new Record(accountUser.getId());
+                        long totalAudioLen = file.length();
+                        long totalDataLen = totalAudioLen + 36;
+                        long longSampleRate = rate;
+                        int channels = 1;
+                        long byteRate = 16 * longSampleRate * channels / 8;
+                        FileInputStream inp = new FileInputStream(file);
+                        FileOutputStream out = new FileOutputStream(record.getFilename());
+                        record.WriteWaveFileHeader(out, totalAudioLen, totalDataLen, longSampleRate, 1, byteRate);
+                        byte[] buff = new byte[16 * 1024];
+                        int sz;
+                        while ((sz = inp.read(buff)) > 0)
+                            out.write(buff, 0, sz);
+                        out.close();
+                        inp.close();
+                        file.delete();
+                        message = new PMessage(true, PMessage.PMESSAGE_MEDIA_TYPE.AUDIO_MESSAGE,
+                                record.getFilename(), timestamp,
+                                PMessage.PMESSAGE_STATUS.STATUS_SENT,
+                                receiverId, senderId);
+                    } else {
+                        JSONObject json = new JSONObject(data);
+                        long timestamp = json.optLong("timestamp", System.currentTimeMillis());
+                        String receiverId = json.optString("receiver", accountUser.getId());
+                        String senderId = json.optString("sender", accountUser.getId());
+                        String text = json.getString("text");
+                        message = new PMessage(true, PMessage.PMESSAGE_MEDIA_TYPE.TEXT_MESSAGE,
+                                text, timestamp,
+                                PMessage.PMESSAGE_STATUS.STATUS_SENT,
+                                receiverId, senderId);
+                    }
+                    if (message != null)
+                        handler.obtainMessage(MSG_POST_CHAT_MESSAGE, message).sendToTarget();
+                } catch (Exception e) {
+                    Log.e(TAG, "Error saving wav file", e);
+                }
+                return null;
+            }
             return null;
         }
 
@@ -163,7 +292,10 @@ public class WearLink extends Service implements IMessenger.MessengerListener
                 json.put("status", "read");
                 break;
             }
-            json.put("text", message.messageBody());
+            if (message.mediaType() == PMessageAbs.PMESSAGE_MEDIA_TYPE.TEXT_MESSAGE)
+                json.put("text", message.messageBody());
+            else if (message.mediaType() == PMessageAbs.PMESSAGE_MEDIA_TYPE.AUDIO_MESSAGE)
+                json.put("audio", message.messageBody());
             return json.toString();
         } catch (JSONException e) {
             Log.e(TAG, "mesage encode error", e);
