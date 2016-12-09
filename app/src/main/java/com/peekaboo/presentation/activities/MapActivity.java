70c562809b72cb91fb8acaba4724aa9477a84c5a
Nikita Romanenko <nikita241296@gmail.com>
2016-12-09 19:08:43 +0200

    DiMA, toolbar now is okay,

--- app/src/main/java/com/peekaboo/presentation/activities/MapActivity.java
+++ app/src/main/java/com/peekaboo/presentation/activities/MapActivity.java
@@ -27,7 +27,6 @@ import com.google.android.gms.maps.model.CameraPosition;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
-
 import com.peekaboo.R;
 
 /**
